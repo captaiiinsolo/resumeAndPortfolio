d6aac01155db89e40cce9e52bbf0cebd46219b1b
captaiiinsolo <solo.d.santos@gmail.com>
2023-09-16 15:34:15 -0700

    style resume and added pageNext and pagePrevious buttons to the resume page

--- ss-resume-and-portfolio/client/src/components/ResumePDF/ResumePDF.js
+++ ss-resume-and-portfolio/client/src/components/ResumePDF/ResumePDF.js
@@ -1,8 +1,12 @@
 import pdf from '../../assets/resume/Resume.pdf';
-import { Grid, Row, Col } from 'rsuite';
+import { useState } from'react';
+import { FlexboxGrid, Container, Grid, Row, Col, Button, IconButton, ButtonGroup, Divider } from 'rsuite';
+import PagePreviousIcon from '@rsuite/icons/PagePrevious';
+import PageNextIcon from '@rsuite/icons/PageNext';
 
 
 import { pdfjs, Document, Page } from'react-pdf';
+import FlexboxGridItem from 'rsuite/esm/FlexboxGrid/FlexboxGridItem';
 
 pdfjs.GlobalWorkerOptions.workerSrc = new URL(
   'pdfjs-dist/build/pdf.worker.min.js',
@@ -11,27 +15,49 @@ pdfjs.GlobalWorkerOptions.workerSrc = new URL(
 
 
 function ResumePDF() {
+
+   const [numPages, setNumPages] = useState(null);
+   const [pageNumber, setPageNumber] = useState(1);
+
+const onDocumentLoadSuccess = ({ numPages }) => {
+  setNumPages(numPages);
+};
+
+const toNextPage = () => {
+   if (pageNumber < numPages) {
+      setPageNumber(pageNumber + 1);
+   }
+};
+
+const toPrevPage = () => {
+   if (pageNumber > 1) {
+      setPageNumber(pageNumber - 1);
+   }
+};
     return (
-     <Grid fluid>
-        <Row>
-        <Document file={pdf}>
-           <Col>
-           <Page 
-            pageNumber={1} 
-            renderAnnotationLayer={false}
-            renderTextLayer={false}
+      <Container>
+           <FlexboxGrid align='middle' justify="center">
+        <FlexboxGrid.Item>
+          <Document file={pdf} onLoadSuccess={onDocumentLoadSuccess}>
+            <Page
+              pageNumber={pageNumber}
+              renderAnnotationLayer={false}
+              renderTextLayer={false}
             />
-           </Col>
-           <Col>
-           <Page 
-            pageNumber={2} 
-            renderAnnotationLayer={false}
-            renderTextLayer={false}
-            />
-           </Col>
-        </Document>
-        </Row>
-     </Grid>
+          </Document>
+        </FlexboxGrid.Item>
+      </FlexboxGrid>
+      <FlexboxGrid justify="center">
+        <FlexboxGrid.Item>
+          <ButtonGroup>
+            <IconButton icon={<PagePreviousIcon />} onClick={toPrevPage} disabled={pageNumber === 1} />
+            <IconButton icon={<PageNextIcon />} onClick={toNextPage} disabled={pageNumber === numPages} />
+          </ButtonGroup>
+        </FlexboxGrid.Item>
+      </FlexboxGrid>
+      </Container>
+
+ 
     );
 }
 
