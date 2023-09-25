e9a6311e35a8d32dbf709d86daa4aea61f37e250
captaiiinsolo <solo.d.santos@gmail.com>
2023-09-25 00:50:17 -0700

    update skills component to include icons for related languages, frameworks and libraries

--- ss-resume-and-portfolio/client/src/components/Biography/Biography.js
+++ ss-resume-and-portfolio/client/src/components/Biography/Biography.js
@@ -5,25 +5,25 @@ export default function Biography() {
   return (
     <Grid fluid>
       <Row style={{ margin: '3rem'}}>
-        <Col lg={11} style={{ margin: '2rem 1.5rem'}}>
+        <Col xxl={11} xl={11} lg={22} md={22} sm={11} xs={22} style={{ margin: '2rem 1.5rem'}}>
           <Panel>
             <Placeholder rows={14} />
           </Panel>
         </Col>
-        <Col lg={11} style={{ margin: '2rem 1.5rem'}}>
-          <Panel>
-            <img src='https://placehold.co/600x400' alt='a placeholder for a bio pic' />
+        <Col xxl={11} xl={11} lg={22} md={22} sm={11} xs={22} style={{ margin: '2rem 1.5rem'}}>
+          <Panel bodyFill>
+            <div style={{ display: 'flex', justifyContent: 'center' }}><img src='https://placehold.co/600x400' alt='a placeholder for a bio pic' /></div>
           </Panel>
         </Col>
       </Row>
 
       <Row style={{ margin: '3rem'}}>
-        <Col lg={11} style={{ margin: '2rem 1.5rem'}}>
-          <Panel>
-            <img src='https://placehold.co/600x400' alt='a placeholder for bio pic number 2' />
+        <Col xxl={11} xl={11} lg={22} md={22} sm={22} xs={22} style={{ margin: '2rem 1.5rem'}}>
+          <Panel bodyFill>
+          <div style={{ display: 'flex', justifyContent: 'center' }}><img src='https://placehold.co/600x400' alt='a placeholder for bio pic number 2' /></div>
           </Panel>
         </Col>
-        <Col lg={11} style={{ margin: '2rem 1.5rem'}}>
+        <Col xxl={11} xl={11} lg={22} md={22} sm={22} xs={22} style={{ margin: '2rem 1.5rem'}}>
           <Panel>
             <Placeholder rows={14} />
           </Panel>
--- ss-resume-and-portfolio/client/src/components/Stats/Stats.js
+++ ss-resume-and-portfolio/client/src/components/Stats/Stats.js
@@ -1,14 +1,16 @@
 import { Grid, Row, Col, Progress, Panel } from 'rsuite';
+import { Icon } from '@rsuite/icons';
+import { SiMongodb, SiExpress, SiReact, SiNodedotjs, SiHtml5, SiJavascript, SiCss3, SiGit } from "react-icons/si";
 
 const skills = [
-    { id: 1, title: "MongoDB", percent: "80", color: "#00ED64", status: "active" },
-    { id: 2, title: "Express.js", percent: "75", color: "#68A063", status: "active" },
-    { id: 3, title: "React.js", percent: "85", color: "61DBFB", status: "active" },
-    { id: 4, title: "Node.js", percent: "75", color: "#3C873A", status: "active" },
-    { id: 5, title: "JavaScript", percent: "80", color: "#F7DF1E", status: "active" },
-    { id: 6, title: "CSS", percent: "90", color: "#264DE4", status: "active" },
-    { id: 7, title: "HTML", percent: "95", color: "#F06529", status: "active" },
-    { id: 8, title: "Git CLI", percent: "85", color: "#171515", status: "active" },
+    { id: 1, title: "MongoDB", percent: "80", color: "#00ED64", status: "active", icon: SiMongodb },
+    { id: 2, title: "Express.js", percent: "75", color: "#68A063", status: "active", icon: SiExpress },
+    { id: 3, title: "React.js", percent: "85", color: "61DBFB", status: "active", icon: SiReact },
+    { id: 4, title: "Node.js", percent: "75", color: "#3C873A", status: "active", icon: SiNodedotjs },
+    { id: 5, title: "JavaScript", percent: "80", color: "#F7DF1E", status: "active", icon: SiJavascript },
+    { id: 6, title: "CSS", percent: "90", color: "#264DE4", status: "active", icon: SiCss3 },
+    { id: 7, title: "HTML", percent: "95", color: "#F06529", status: "active", icon: SiHtml5 },
+    { id: 8, title: "Git CLI", percent: "85", color: "#171515", status: "active", icon: SiGit },
     // { id: 9, title: "MySQL", percent: "80", color: "#00758F" },
   ];
 
@@ -23,7 +25,8 @@ export default function Stats() {
   const skillsList = skills.map((skill) => (
     <div style={style}>
       <Progress.Circle percent={skill.percent} gapPosition="bottom" strokeColor={skill.color} status={skill.status} />
-      <p style={{ marginTop: "1rem" }}>{skill.title}</p>
+      <p style={{ margin: "1rem" }}>{skill.title}</p>
+      <Icon as={skill.icon} size='2em' style={{ color: skill.color}} />
     </div>
   ));
 
@@ -31,7 +34,7 @@ export default function Stats() {
     <Grid fluid>
       <Row style={{ margin: "3rem" }}>
         <Col lg={22} style={{ margin: "1rem 1.5rem" }}>
-          <Panel shaded style={{ display: 'flex', justifyContent: 'center' }}>{skillsList}</Panel>
+          <Panel shaded style={{ display: 'flex', justifyContent: 'space-around' }}>{skillsList}</Panel>
         </Col>
       </Row>
     </Grid>
