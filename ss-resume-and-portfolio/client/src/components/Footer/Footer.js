9d07f6b9a57fbddfab1655e7d29b0ea6cb966d57
captaiiinsolo <solo.d.santos@gmail.com>
2023-11-08 19:24:43 -0800

    start writing code for footer. Need to align footer nav to left side of screen.

--- ss-resume-and-portfolio/client/src/components/Footer/Footer.js
+++ ss-resume-and-portfolio/client/src/components/Footer/Footer.js
@@ -1,42 +1,32 @@
 import React from 'react';
-import { Drawer, IconButton, Grid, Row, Col, Button } from 'rsuite';
-import AngleUpIcon from '@rsuite/icons/legacy/AngleUp';
+import { Nav, Grid, Row, Col, Button } from 'rsuite';
 
 export default function Footer() {
-  const [open, setOpen] = React.useState(false);
-
-  const [placement, setPlacement] = React.useState();
-
-  const handleOpen = (key) => {
-    setOpen(true);
-    setPlacement(key);
+  const FooterNav = ({ active, onSelect, ...props }) => {
+    return (
+      <Nav
+        {...props}
+        vertical
+        activeKey={active}
+        onSelect={onSelect}
+      >
+        <Nav.Item href='/' eventKey="1">Home</Nav.Item>
+        <Nav.Item href='/resume' eventKey="2">Resumé</Nav.Item>
+        <Nav.Item href='/projects' eventKey="3">Projects</Nav.Item>
+        <Nav.Item href='/contact-me' eventKey="4">Contact Me</Nav.Item>
+      </Nav>
+    );
   };
+  
+
+  const [active, setActive] = React.useState('home');
 
   return (
-    <Grid fluid>
-      <Row>
-        <Col>
-          <div>
-            <IconButton
-              icon={<AngleUpIcon />}
-              onClick={() => handleOpen("bottom")}
-              appearance="link"
-            />
 
-            <Drawer
-              size="xs"
-              placement={placement}
-              open={open}
-              onClose={() => setOpen(false)}
-            >
-              <Drawer.Actions>
-                <Button onClick={() => setOpen(false)}>Cancel</Button>
-                <Button onClick={() => setOpen(false)} appearance="primary">
-                  Confirm
-                </Button>
-              </Drawer.Actions>
-            </Drawer>
-          </div>
+    <Grid fluid>
+      <Row style={{ margin: "2rem" }}>
+        <Col lg={22} style={{ margin: "1rem 1.5rem"}}>
+         <FooterNav active={active} onSelect={setActive} appearance='sublte' />
         </Col>
       </Row>
     </Grid>
